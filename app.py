253514661b460b2fbbe8b9d62516fb844a1eb396
MayMay <135733303+MayMatilde@users.noreply.github.com>
2024-12-05 14:41:18 -0300

    Add files via upload
    Coisinhas básicas do banco de dados

--- app.py
+++ app.py
@@ -1,28 +1,55 @@
-from flask import Flask, jsonify
-from models.user import User, db
-from database import init_db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dados.db"
+db = SQLAlchemy()
+db.init_app(app)
 
-init_db(app)
+class Usuarios(db.Model):
+    __tablename__ = 'user'
 
-@app.route('/add')
-def add():  #não é necessário passar o id, pois o bd já entende
-    user = User(nome = "May", email = "may@maymay.com")
-    db.session.add(user)
-    db.session.commit()
-    return 'Adicionado com sucesso!'
+    id = db.Column(db.Integer(), primary_key=True)
+    nome = db.Column(db.String(40), nullable=False, unique=True)
 
-@app.route('/ver')
-def ver():
-    users = User.query.all()
-    listaUser = []
+    def __repr__(self):
+        return f"<{self.nome}"
 
-    for user in users: 
-        listaUser.append(user.toJson())
-
-    return jsonify(listaUser)  #retornando a minha lista para json a cada rota "add" atualizada
-
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run()
+    with app.app_context():
+        #----adicionando dados---
+        
+        #user = Usuarios(nome='Baby Shark')
+        #db.session.commit()  #não se esquecer dele
+        #db.create_all()
+
+        #---obtendo dados--
+        
+        #usuarios = db.session.query(Usuarios).all() #query= requisão para o banco de dados  #all()= tudo da tabela 
+        #for user in  usuarios:
+        #   print(f"Usuario: {user.id }")
+        #print(usuarios)
+
+        #---filtrando apenas um dado do banco---
+       
+       # user = db.session.query(Usuarios).filter_by(id=3).first()
+       # print(user)
+
+       #---alterando os dados--- 
+        
+        #user = db.session.query(Usuarios).filter_by(id=3).first()
+        #user.nome = "Maria Helena"
+        #db.session.commit()
+
+        #---deletando dados no banco---
+        user = db.session.query(Usuarios).filter_by(id=3).first()
+        db.session.delete(user)
+        db.session.commit()
+
+        
+
+    app.run(debug=True)
